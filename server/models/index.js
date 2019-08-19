28716f55b1c5ac96a984ebe4fe7674e03a5887b2
cuongvn <cuongvn@inet.vn>
2019-08-19 12:38:33 +0700

    update

--- server/models/index.js
+++ server/models/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const { DOCKER, MONGO_URL, MONGO_DOCKER_URL } = process.env;
 const docker = DOCKER === '1';
 mongoose.connect(
-	docker ? process.env.MONGO_URL : process.env.MONGO_DOCKER_URL,
+	docker ? MONGO_DOCKER_URL : MONGO_URL,
 	{ useNewUrlParser: true },
 	() => {
 		console.log('mongodb connected');
