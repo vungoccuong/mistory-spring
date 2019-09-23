1f5fd56a4487e891d4013387033883b9b3725bb0
hirosume <cuongvn@inet.vn>
2019-09-23 17:39:08 +0700

    hmmm

--- server/src/main/java/com/ltm2019/mistory/entities/LogoutResponse.java
+++ server/src/main/java/com/ltm2019/mistory/entities/LogoutResponse.java
@@ -3,4 +3,9 @@ package com.ltm2019.mistory.entities;
 import com.ltm2019.mistory.controller.Responsible;
 
 public class LogoutResponse implements Responsible {
+    private String message = "Đăng xuất thành công";
+
+    public String getMessage() {
+        return message;
+    }
 }
